5cfa36d703432eb3725833e47e434102b990f6d7
ak4rnis <pieero.flores500@gmail.com>
2023-04-04 19:23:48 -0400

    Mensaje de confirmación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
