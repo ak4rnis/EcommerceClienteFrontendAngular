0088951753744d6e9f6b46ae6f950fe8716693f9
Pieero Flores <pieero.flores500@gmail.com>
2023-06-14 01:14:27 -0400

    frontend cliente historial de venta del usuario

--- src/app/components/usuario/ordenes/detalle-orden/detalle-orden.component.ts
+++ src/app/components/usuario/ordenes/detalle-orden/detalle-orden.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GLOBAL } from 'src/app/services/GLOBAL';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-detalle-orden',
   templateUrl: './detalle-orden.component.html',
   styleUrls: ['./detalle-orden.component.css']
 })
-export class DetalleOrdenComponent {
+export class DetalleOrdenComponent implements OnInit {
+  public url:any;
+  public token:any;
+  public orden:any = {};
+  public detalles: Array<any> = [];
+  public load_data:Boolean = true;
+  public id:any;
+  constructor(private _clienteServcie:ClienteService, private _route:ActivatedRoute){
+    this.token = localStorage.getItem('token');
+    this.url = GLOBAL.url;
+    this._route.params.subscribe(
+      params => {
+        this.id = params['id'];
+        this._clienteServcie.obtener_detalles_ordenes_cliente(this.id,this.token).subscribe(
+          response => {
+            if(response.data != undefined){
+              this.orden = response.data;
+              this.detalles = response.detalles;
+            }else{
+              this.orden = undefined;
 
+            }
+            
+          }
+        )
+      }
+    )
+
+  }
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -117,7 +117,15 @@ export class ClienteService {
     return this._http.get(this.url+'validar_cupon_admin/'+cupon,{headers:headers});
   }
 
-  
+  obtener_ordenes_cliente(id:any,token:any):Observable<any>{
+    let headers = new HttpHeaders({'Content-Type':'application/json','Authorization':token});
+    return this._http.get(this.url+'obtener_ordenes_cliente/'+id,{headers:headers});
+  }
+
+  obtener_detalles_ordenes_cliente(id:any,token:any):Observable<any>{
+    let headers = new HttpHeaders({'Content-Type':'application/json','Authorization':token});
+    return this._http.get(this.url+'obtener_detalles_ordenes_cliente/'+id,{headers:headers});
+  }
 
   
 
