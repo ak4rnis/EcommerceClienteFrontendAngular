f8f4b4f39cd579e79e8ecfebb577423031786a9f
Pieero Flores <pieero.flores500@gmail.com>
2023-06-15 01:06:22 -0400

    agregar modal review

--- src/app/components/usuario/ordenes/detalle-orden/detalle-orden.component.ts
+++ src/app/components/usuario/ordenes/detalle-orden/detalle-orden.component.ts
@@ -3,7 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { StarRatingComponent } from 'ng-starrating';
 import { GLOBAL } from 'src/app/services/GLOBAL';
 import { ClienteService } from 'src/app/services/cliente.service';
-
+declare var iziToast:any;
+declare var $:any;
 @Component({
   selector: 'app-detalle-orden',
   templateUrl: './detalle-orden.component.html',
@@ -24,20 +25,7 @@ export class DetalleOrdenComponent implements OnInit {
     this._route.params.subscribe(
       params => {
         this.id = params['id'];
-        this._clienteServcie.obtener_detalles_ordenes_cliente(this.id,this.token).subscribe(
-          response => {
-            if(response.data != undefined){
-              this.orden = response.data;
-              this.detalles = response.detalles;
-              console.log(this.orden);
-              console.log(this.detalles);
-            }else{
-              this.orden = undefined;
-
-            }
-            
-          }
-        )
+        this.init_data();
       }
     )
 
@@ -45,8 +33,74 @@ export class DetalleOrdenComponent implements OnInit {
   ngOnInit(): void {
     
   }
+  init_data(){
+    this._clienteServcie.obtener_detalles_ordenes_cliente(this.id,this.token).subscribe(
+      response => {
+        if(response.data != undefined){
+          this.orden = response.data;
+          this.detalles = response.detalles;
+          console.log(this.orden);
+          console.log(this.detalles);
+        }else{
+          this.orden = undefined;
+
+        }
+        
+      }
+    )
+  }
+
+  onModal(item:any){
+    this.review = {};
+    this.review.producto = item.producto._id;
+    this.review.cliente = item.cliente,
+    this.review.venta = this.id;
+  }
 
   onRate($event:{oldValue:number, newValue:number, starRating:StarRatingComponent}){
 
   }
+
+  emitir(id:any){
+    if(this.review.review){
+      if(this.totalstar && this.totalstar >= 0){
+        this.review.estrellas = this.totalstar;
+        this._clienteServcie.emitir_review_producto_cliente(this.review,this.token).subscribe(
+          response => {
+            iziToast.show({
+              title: 'SUCCESS',
+              titleColor: '#1DC74C',
+              color: '#FFF',
+              class: 'text-success',
+              position: 'topRignt',
+              message: 'Se emitio correctamente la reseña.'
+            });
+            $('#review-'+id).modal('hide');
+            $('.modal-backdrop').removeClass('show');
+            this.init_data();
+          }
+        )
+      }else{
+        iziToast.show({
+          title: 'ERROR',
+          titleColor: '#FF0000',
+          color: '#FFF',
+          class: 'text-danger',
+          position: 'topRignt',
+          message: 'Selecione un numero de estrellas'
+        });
+      }
+    }else{
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FF0000',
+        color: '#FFF',
+        class: 'text-danger',
+        position: 'topRignt',
+        message: 'Ingrese un mensaje de la reseña'
+      });
+    }
+  }
+
+
 }
